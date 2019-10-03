cf9409573fc81e762d0290eb9d9bca0df490a825
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2019-10-03 16:10:12 -0500

    Javascript Activity

---
+++ Module-6/javascript.js
@@ -0,0 +1,24 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height= "250px";
+});
+
+document.getElementById("button2").addEventListener("click", function()
+{
+    box.style.backgroundColor = "blue";
+});
+
+document.getElementById("button3").addEventListener("click", function()
+{
+    box.style.opacity = 0;
+});
+
+document.getElementById("button4").addEventListener("click", function()
+{
+    box.style.height= "150px";
+    box.style.opacity = 1;
+    box.style.backgroundColor = "orange";
+    
+
+});
+
+
